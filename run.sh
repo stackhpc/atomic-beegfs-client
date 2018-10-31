d574534626e7c63c0a77af5d0435833c6ae3a06f
Bharat Kunwar <b.kunwar@gmail.com>
2018-10-31 18:19:30 +0000

    Wait for host to be up and enter daemon mode

--- run.sh
+++ run.sh
@@ -3,4 +3,12 @@ BEEGFS_MGMTD_HOST="${BEEGFS_MGMTD_HOST:-localhost}"
 echo "
 # BeeGFS management host
 $BEEGFS_MGMTD_HOST beegfs-mgmtd-host" >> /etc/hosts
-/etc/init.d/beegfs-helperd start && /etc/init.d/beegfs-client start
+while ! ping -c 1 -W 1 $BEEGFS_MGMTD_HOST; do
+    echo "Waiting for $BEEGFS_MGMTD_HOST - network interface might be down..."
+    sleep 1
+done
+/etc/init.d/beegfs-helperd start && sleep 5 && /etc/init.d/beegfs-client start
+while 1; do
+    echo "Entering daemon mode..."
+    sleep 1
+done
