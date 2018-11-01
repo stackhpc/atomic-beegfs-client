1619bce62125cd94a1056c242a41606d78bbdd6d
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-01 06:33:22 +0000

    Sleep infinitely

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#! /bin/env bash
+#! /bin/env sh
 BEEGFS_MGMTD_HOST="${BEEGFS_MGMTD_HOST:-localhost}"
 echo "
 # BeeGFS management host
@@ -7,8 +7,8 @@ while ! ping -c 1 -W 1 $BEEGFS_MGMTD_HOST; do
     echo "Waiting for $BEEGFS_MGMTD_HOST - network interface might be down..."
     sleep 1
 done
-/etc/init.d/beegfs-helperd start && sleep 5 && /etc/init.d/beegfs-client start
-while 1; do
-    echo "Entering daemon mode..."
-    sleep 1
-done
+
+/etc/init.d/beegfs-helperd start && /etc/init.d/beegfs-client start
+echo "Entering daemon mode... (press Ctrl+C)" && sleep infinity
+umount /mnt/beegfs && /etc/init.d/beegfs-client stop /etc/init.d/beegfs-helperd stop
+echo "Beegfs has stopped... (press Ctrl+C)" && sleep infinity
