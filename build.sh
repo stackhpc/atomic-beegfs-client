8fb401ed3bdd4204732d5442b90fa316ef7ed7ae
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-02 15:30:07 +0000

    Trivial renames

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ export BEEGFS_MGMTD_HOST=10.60.253.20
 
 docker build --build-arg BEEGFS_VERSION=${BEEGFS_VERSION} \
     --build-arg BEEGFS_KERNEL_VERSION=${BEEGFS_KERNEL_VERSION} \
-    -t brtknr/beegfs-client:${BEEGFS_VERSION} .
-
-docker push brtknr/beegfs-client:${BEEGFS_VERSION}
-
-docker run --name beegfs-client -d \
-     -e "BEEGFS_MGMTD_HOST=${BEEGFS_MGMTD_HOST}" --rm --privileged \
-     --network=host --mount type=bind,src=/mnt/beegfs,target=/mnt/beegfs,bind-propagation=shared \
-     brtknr/beegfs-client:${BEEGFS_VERSION}
+    -t brtknr/beegfs-client:${BEEGFS_VERSION} . && \
+    docker push brtknr/beegfs-client:${BEEGFS_VERSION} && \
+    docker run --name beegfs-client -d \
+        -e "BEEGFS_MGMTD_HOST=${BEEGFS_MGMTD_HOST}" --rm --privileged \
+        --network=host --mount type=bind,src=/mnt/beegfs,target=/mnt/beegfs,bind-propagation=shared \
+        brtknr/beegfs-client:${BEEGFS_VERSION}
